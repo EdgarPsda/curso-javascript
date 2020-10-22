2c68b8dabd8d7d88faf5a9625b0282367a7d45b3
unknown <edgar.psda@gmail.com>
2020-10-22 09:57:57 -0700

    Operadores parte 1.

---
+++ operadores.js
@@ -0,0 +1,17 @@
+// Operadores Aritmeticos
+// Adición
+// Sustracción
+// Multiplicación
+// División
+// Modulo
+// Incremento
+// Decremento
+
+// Operadores de Asignación
+//  =
+//  +=
+//  -=
+//  *=
+//  /=
+//  %=
+//  **=
