1a5218b8a2642571f54a88775b1fa090b98c2a77
unknown <edgar.psda@gmail.com>
2020-10-22 16:52:42 -0700

    Operadores de comparacion y logicos.

---
+++ operadores2.js
@@ -0,0 +1,33 @@
+var x = 5;
+var y = 8;
+// Operadores de Comparacion
+// == Valida valor
+// console.log(x == 5);
+// === Valida valor y el tipo de dato
+// console.log(x === 5);
+// !=
+// console.log(x != 3);
+// !==
+// console.log(x !== 5);
+// >
+// console.log(x > 3);
+// <
+// console.log(x < 7);
+// >=
+// console.log(x >= 2);
+// <=
+// console.log(x <= 5);
+
+// ? Operador Ternario
+// var variable = (condicion) ? sentencia1 : sentencia2;
+// var respuesta = (x == 5) ? "Si es igual a 5" : "No es igual a 5";
+// console.log(respuesta);
+
+// Operadores Logicos
+// && AND
+// console.log(x == 3 && y == 3);
+// || OR
+// console.log(x == 5 || y == 8);
+// ! NOT
+console.log(!(x == 4 && y == 8));
+
